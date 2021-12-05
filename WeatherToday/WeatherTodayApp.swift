32110d9e034b4989129d74ee55dd66e4860619e6
Festus obi <fessy_wonder@yahoo.co.uk>
2021-12-05 22:06:22 +0100

    Initial Commit

---
+++ WeatherToday/WeatherTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTodayApp.swift
+//  WeatherToday
+//
+//  Created by Festus Obi on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
