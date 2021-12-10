4b4819c9589cb680f605f97ab436162be796f6d2
Festus obi <fessy_wonder@yahoo.co.uk>
2021-12-10 06:33:16 +0100

    added weather row component

--- WeatherToday/Views/WeatherUIView.swift
+++ WeatherToday/Views/WeatherUIView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct WeatherUIView: View {
     var weather: ResponseBody
+    private let imageURL: String = "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png"
     
     var body: some View {
         ZStack(alignment: .leading){
@@ -45,7 +46,7 @@ struct WeatherUIView: View {
                     Spacer()
                         .frame(height: 80)
                     
-                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
+                    AsyncImage(url: URL(string: imageURL)) { image in
                         image
                             .resizable()
                             .aspectRatio(contentMode: .fit)
@@ -54,12 +55,39 @@ struct WeatherUIView: View {
                         ProgressView()
                     }
                     
-                    //Spacer()
+                    Spacer()
                 }
                 .frame(maxWidth: .infinity)
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack{
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20){
+                    Text("Weather Now")
+                        .bold()
+                        .padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer", name: "Min temp", value: (weather.main.tempMin.roundDouble() + "°"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer", name: "Max temp", value: (weather.main.tempMax.roundDouble() + "°"))
+                    }
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value: (weather.wind.speed.roundDouble() + "m/s"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Humidity", value: (weather.main.humidity.roundDouble() + "%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
